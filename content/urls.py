cdb3f9a8c7db446b86d589e3e219f863c61e70fc
hanmariyang <hanmari.code@gmail.com>
2022-10-03 22:44:21 +0900

    게시글 작성
    home - 게시글 작성

--- content/urls.py
+++ content/urls.py
@@ -1,8 +1,10 @@
 # tweet/urls.py
 from django.urls import path
 from . import views
+from .views import UploadFeed
 
 urlpatterns = [
     path('', views.home, name='home'), # 127.0.0.1:8000 과 views.py 폴더의 home 함수 연결
-    path('content/', views.content, name='content') # 127.0.0.1:8000/tweet 과 views.py 폴더의 tweet 함수 연결
+    path('content/', views.content, name='content'), # 127.0.0.1:8000/tweet 과 views.py 폴더의 tweet 함수 연결
+    path('content/upload', UploadFeed.as_view()),
 ]
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
+from .settings import MEDIA_URL, MEDIA_ROOT
+from content.views import UploadFeed
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('content.urls')),
     path('', include('user.urls')),
+    path('content/upload', UploadFeed.as_view())
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
